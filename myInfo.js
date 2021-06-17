7961c8fafc66cb75ea2997edfb20b3e1d680f6f3
teetia1 <85227528+teetia1@users.noreply.github.com>
2021-06-17 20:18:05 +0100

    Add files via upload

---
+++ myInfo.js
@@ -0,0 +1,14 @@
+let myName = "Isaac";
+let gender = "Male";
+let nationality = "Nigerian";
+const dateOfBirth = "24/05";
+let phoneNo = "08136244387";
+let email = "isaactoluwa@outlook.com";
+let github = "https://github.com/teetia1";
+let profession = "fullstack developer, app developer, UI/UX,"
+console.log("My name is", myName);
+console.log("I am a", gender);
+console.log("I am a", nationality);
+console.log("I was born on", dateOfBirth);
+console.log("You can reach me on", phoneNo, ",", email, "and", github);
+console.log("I am an aspiring", profession);
