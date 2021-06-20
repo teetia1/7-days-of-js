2a437aaa44dca332cd86146aa055e44989d4e1ee
teetia1 <85227528+teetia1@users.noreply.github.com>
2021-06-20 15:41:13 +0100

    Add files via upload

---
+++ data-type-assignment.js
@@ -0,0 +1,28 @@
+//Task 1
+//1
+let userName = "Sophia";    
+console.log('hello ${3}');          //hello ${3}
+
+//2
+console.log('hello ${"userName",}'); //hello ${"userName"}
+
+//3
+alert('hello ${userName}');         //hello ${userName}
+
+
+//Task 2
+//1
+let price = prompt("What is the price of the Item", "<price goes here>");
+console.log("The price of the item is", price, "dollars");
+
+//2
+//typeof{price} is object
+
+//3
+// it will concatenate
+
+
+//Task 3
+let string = 'Although the day was "sunny",' + " Sophie thought it wise to take John's umbrella";
+console.log(string)
+
